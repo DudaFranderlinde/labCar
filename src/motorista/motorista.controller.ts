c3992bed7954871bcab92a1d390eb4467456ac69
DudaFranderlinde <108769434+DudaFranderlinde@users.noreply.github.com>
2022-11-06 22:00:00 -0300

    Ajustes

--- src/motorista/motorista.controller.ts
+++ src/motorista/motorista.controller.ts
@@ -12,10 +12,9 @@ export class MotoristaController{
 
     @Get()
     public async getMotoristas(@Query('name') name = "", @Query('page') page = 0, @Query('size') size = 10) {
-        const motorista = await this.service.getListaMotoristas(page,size)
-        
+        const motorista = await this.service.getListaMotoristas(page,size);
         if(name !== ""){
-           return await this.service.getMotoristaName(name)
+           return await this.service.getMotoristaName(name);
         }
         return motorista;
     }
@@ -28,7 +27,7 @@ export class MotoristaController{
 
     @Post('')
     public async createMotorista(@Body() motorista: Motorista): Promise<NestResponse>{
-        const motoristaCriado = await this.service.createMotorista(motorista)
+        const motoristaCriado = await this.service.createMotorista(motorista);
 
         return new NestResponseBuilder()
         .withStatus(HttpStatus.CREATED)
@@ -39,20 +38,20 @@ export class MotoristaController{
 
     @Put(':id')
     public async updateMotorista(@Param('id') id, @Body() body: updateMotoristaDto){
-        const updateMotorista = await this.service.updateCadastro(id, body)
+        const updateMotorista = await this.service.updateCadastro(id, body);
         return updateMotorista;
     }
 
     @Patch(':id')
     public async updateStatusMotorista(@Param('id') id, @Body() body : updateStatusMotoristaDto){
-        const updateStatusMotorista = await this.service.updateStatus(id, body.status)
+        const updateStatusMotorista = await this.service.updateStatus(id, body.status);
         return updateStatusMotorista;
     }
 
     @Delete(':id')
     public async deleteMotorista(@Param() params){
-        const motoristas = await this.service.getMotoristaID(params.id)
-        await this.service.deleteMotorista(params.id)
+        const motoristas = await this.service.getMotoristaID(params.id);
+        await this.service.deleteMotorista(params.id);
         return motoristas;
     }
 }
